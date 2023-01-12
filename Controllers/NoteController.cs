cab4752bd0811e2686b969ea70a69208dac1a808
Luis Mendoza <luis23luis33@gmail.com>
2023-01-12 15:20:56 -0500

    creando funciones dentro del controlador de notas

--- Controllers/NoteController.cs
+++ Controllers/NoteController.cs
@@ -1,10 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using AppContactos.Models;
+using AppContactos.Servicios.Contrato;
+
 namespace AppContactos.Controllers
 {
     public class NoteController : Controller
     {
-        public IActionResult Index()
+        private readonly INoteService _noteService;
+
+        public NoteController(INoteService noteService)
+        {
+            _noteService = noteService;
+        }
+
+        public IActionResult SaveNote()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        
+        public IActionResult DetailsNote()
         {
             return View();
         }
