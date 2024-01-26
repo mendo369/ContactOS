0325d34808d04694955817dbc23812e615d21fc7
Luis Mendoza <luis23luis33@gmail.com>
2024-01-26 08:21:44 -0500

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules').then((m) => m.HomeModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./modules').then((m) => m.AuthModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
