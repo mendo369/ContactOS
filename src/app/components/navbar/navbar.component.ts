98b290801ca48fbcaff3eb632f42611e3a6a7cee
Luis Mendoza <luis23luis33@gmail.com>
2024-01-26 13:26:09 -0500

    get user info (name)

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { StorageService } from '../../storage.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  constructor(private storageService: StorageService) {}
+
+  user = this.storageService.getItem('user');
+}
