ef7adc2373bf800e6a2ac9897ca375719496452b
Luis Mendoza <luis23luis33@gmail.com>
2024-01-27 11:57:24 -0500

    add contacts

--- src/app/modules/contacts/contacts.component.ts
+++ src/app/modules/contacts/contacts.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ContactsService } from './contacts.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contacts',
@@ -8,16 +9,38 @@ import { ContactsService } from './contacts.service';
 })
 export class ContactsComponent {
   contacts: any[] = [];
+  addContact = false;
+  formContact: FormGroup;
 
-  constructor(private contactsService: ContactsService) {}
+  constructor(private contactsService: ContactsService) {
+    this.formContact = new FormGroup({
+      name: new FormControl('', Validators.required),
+      lastname: new FormControl('', Validators.required),
+      phone: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      address: new FormControl('', Validators.required),
+    });
+  }
 
   ngOnInit(): void {
     this.getContacts();
+  }
+
+  displayAddContact() {
+    this.addContact = !this.addContact;
   }
 
   getContacts() {
     this.contactsService
       .getContacts()
       .subscribe((contacts) => (this.contacts = contacts.value));
+  }
+
+  onSaveContact() {
+    this.contactsService
+      .create(this.formContact.value)
+      .subscribe((res) =>
+        res.status ? (this.addContact = false) : (this.addContact = true)
+      );
   }
 }
--- src/app/modules/contacts/contacts.module.ts
+++ src/app/modules/contacts/contacts.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ContactsRoutingModule } from './contacts-routing.module';
 import { ContactsComponent } from './contacts.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ContactsComponent],
-  imports: [CommonModule, ContactsRoutingModule],
+  imports: [CommonModule, ContactsRoutingModule, ReactiveFormsModule],
 })
 export class ContactsModule {}
