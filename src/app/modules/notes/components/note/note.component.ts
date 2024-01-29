888f276af0f765c9175a219d3ae44a57ad2fb03f
Luis Mendoza <luis23luis33@gmail.com>
2024-01-29 14:27:15 -0500

    Notes CRUD

--- src/app/modules/notes/components/note/note.component.ts
+++ src/app/modules/notes/components/note/note.component.ts
@@ -1,10 +1,83 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NotesService } from '../../notes.service';
 
 @Component({
   selector: 'app-note',
   templateUrl: './note.component.html',
-  styleUrl: './note.component.css'
+  styleUrl: './note.component.css',
 })
 export class NoteComponent {
+  noteId: number;
+  formNote: FormGroup;
 
+  constructor(
+    private route: ActivatedRoute,
+    private noteService: NotesService,
+    private router: Router
+  ) {
+    this.noteId = 0;
+
+    this.formNote = new FormGroup({
+      title: new FormControl('', [
+        Validators.required,
+        Validators.minLength(1),
+      ]),
+      contentNote: new FormControl('', [
+        Validators.required,
+        Validators.minLength(1),
+      ]),
+    });
+  }
+
+  ngOnInit(): void {
+    try {
+      this.noteId = parseInt(this.route.snapshot.paramMap.get('id')!);
+      this.getNote(this.noteId);
+    } catch (error) {
+      this.noteId = 0;
+    }
+  }
+
+  getNote(id: number) {
+    this.noteService.getNote(id).subscribe((res) => {
+      if (res.status) {
+        this.formNote.patchValue({
+          title: res.value.title,
+          contentNote: res.value.contentNote,
+        });
+        return;
+      }
+      alert('error');
+    });
+  }
+
+  onSaveNote() {
+    this.noteService
+      .update(this.formNote.value, this.noteId)
+      .subscribe((res) => {
+        if (res.status) {
+          alert('Nota actualizada');
+          this.router.navigate(['/notes']);
+          return;
+        }
+        alert('error');
+      });
+  }
+
+  onDeleteNote() {
+    this.noteService.delete(this.noteId).subscribe((res) => {
+      if (res.status) {
+        alert('Nota eliminada');
+        this.router.navigate(['/notes']);
+        return;
+      }
+      alert('error');
+    });
+  }
+
+  onCancel() {
+    this.router.navigate(['/notes']);
+  }
 }
--- src/app/modules/notes/notes.service.ts
+++ src/app/modules/notes/notes.service.ts
@@ -25,11 +25,39 @@ export class NotesService {
     return this.http.get<any>(this.baseUrl, { headers });
   }
 
+  getNote(id: number) {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.token}`, // Add the authentication header
+    });
+
+    return this.http.get<any>(`${this.baseUrl}/${id}`, { headers });
+  }
+
   create(formValue: any) {
     const headers = new HttpHeaders({
       Authorization: `Bearer ${this.token}`, // Add the authentication header
     });
 
     return this.http.post<any>(this.baseUrl, formValue, { headers });
+  }
+
+  update(formValue: any, noteId: number) {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.token}`, // Add the authentication header
+    });
+
+    return this.http.put<any>(`${this.baseUrl}/${noteId}`, formValue, {
+      headers,
+    });
+  }
+
+  delete(noteId: number) {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.token}`, // Add the authentication header
+    });
+
+    return this.http.delete<any>(`${this.baseUrl}/${noteId}`, {
+      headers,
+    });
   }
 }
